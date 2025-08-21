0fef1c4a1083c62cd9954c932b42f8d7664db6c9
Jonathan Yeo <joyekajie@gmail.com>
2025-08-21 17:00:51 +0800

    chore: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'FPS Portfolio',
+  title: 'jonyeokj',
   description:
     'A first-person shooter portfolio showcasing my experiences and projects',
 };
